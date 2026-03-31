1f3a3655446f5a48ae2cb68bfe27b91e995b705c
TDLWP <TDLWP@users.noreply.github.com>
2026-03-31 12:17:32 -0700

    Transform Retro Sokoban into a full PWA (Offline, Installable app)

---
+++ Sokoban/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'sokoban-retro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Retourne depuis le cache (hors-ligne)
+        }
+        return fetch(event.request); // Sinon requête réseau
+      })
+  );
+});
